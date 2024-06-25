2a85cba7e78f04fdcc41abb23461de3e032fe1c5
bikram <6bikramsutar@gmail.com>
2024-06-25 07:19:18 +0530

    testing build & npm install

---
+++ built-package/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// Import components here to make them available for installation
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Button = void 0;
+var Button_1 = require("./src/stories/Button");
+Object.defineProperty(exports, "Button", { enumerable: true, get: function () { return Button_1.Button; } });
