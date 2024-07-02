cbe2c5308a825cad24ca18667603d26845a40277
priyanshu-09 <priyanshushukla9801@gmail.com>
2024-07-02 16:30:25 +0530

    ChatInput Added

---
+++ src/components/ChatInput.tsx
@@ -0,0 +1,123 @@
+import { PaperPlaneTilt } from "@phosphor-icons/react";
+import { Button, Card, Flex, InputRef, Typography } from "antd";
+import TextArea from "antd/es/input/TextArea";
+import { KeyboardEvent, LegacyRef } from "react";
+
+export interface ChatInputProps {
+  inputRef?: LegacyRef<InputRef> | null;
+  placeholder?: string;
+  userQuery: string;
+  setUserQuery: React.Dispatch<React.SetStateAction<string>>;
+  handleSendMessage: () => void;
+  isFollowupDisabled?: boolean;
+}
+
+const ChatInput: React.FC<ChatInputProps> = ({
+  inputRef,
+  placeholder = "Ask me anything about your product data",
+  userQuery,
+  setUserQuery,
+  handleSendMessage,
+
+  isFollowupDisabled = false,
+}: ChatInputProps) => {
+  const isButtonDisabled = userQuery.trim().length === 0 || isFollowupDisabled;
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.stopPropagation();
+      e.preventDefault();
+      if (!isButtonDisabled) {
+        handleSendMessage();
+      }
+    }
+  };
+
+  return (
+    <Flex
+      vertical
+      style={{
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        marginLeft: "auto",
+        marginRight: "auto",
+        width: "60vw",
+      }}
+    >
+      <div
+        style={{
+          backgroundImage: `linear-gradient(180deg, transparent 23%, var(--background) 97%)`,
+          height: "6vh",
+        }}
+      />
+      <Card
+        size="small"
+        bordered
+        style={{
+          width: "100%",
+          borderBottomLeftRadius: 0,
+          borderBottomRightRadius: 0,
+        }}
+        styles={{
+          body: {
+            padding: 16,
+          },
+        }}
+      >
+        <Flex vertical gap={16}>
+          <Flex align="flex-start" gap={12}>
+            <TextArea
+              ref={inputRef}
+              value={userQuery}
+              onChange={(e) => setUserQuery(e.target.value)}
+              autoSize={{ minRows: 1, maxRows: 8 }}
+              size="large"
+              variant="borderless"
+              placeholder={placeholder}
+              onKeyDown={handleKeyDown}
+            />
+
+            <Button
+              disabled={isButtonDisabled}
+              size="large"
+              type="primary"
+              shape="circle"
+              onClick={handleSendMessage}
+              icon={<PaperPlaneTilt weight="bold" />}
+            />
+          </Flex>
+          <Flex align="flex-end" justify="flex-end">
+            <Typography.Text
+              type="secondary"
+              style={{
+                fontWeight: 500,
+                fontSize: "0.75rem",
+
+                visibility: userQuery.trim().length > 0 ? "visible" : "hidden",
+              }}
+            >
+              Use{" "}
+              <Typography.Text
+                type="secondary"
+                style={{
+                  backgroundColor: "var(--background)",
+                  fontWeight: 500,
+                  fontSize: "0.75rem",
+                  padding: "3px 6px",
+                  borderRadius: 4,
+                }}
+              >
+                shift + enter
+              </Typography.Text>{" "}
+              for new line
+            </Typography.Text>
+          </Flex>
+        </Flex>
+      </Card>
+    </Flex>
+  );
+};
+
+export default ChatInput;
---
+++ src/stories/ChatInput.stories.ts
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import ChatInput from "../components/ChatInput";
+
+const ActionsData = {
+  handleSendMessage: fn(),
+  setUserQuery: fn(),
+};
+
+const meta: Meta<typeof ChatInput> = {
+  title: "Native App/ChatInput",
+  component: ChatInput,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+
+  args: {
+    ...ActionsData,
+  },
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ChatInput>;
+
+export const Default: Story = {
+  args: {
+    userQuery: "Somethign",
+    placeholder: "Ask me anything about your product data",
+  },
+
+  // render: function Render(args) {
+  //     const [{ userQuery }, updateArgs] = useArgs();
+
+  //     function sendMessage() {
+
+  //     }
+  //     function updateQuery(e) {
+  //         updateArgs({ userQuery: e.target.value });
+  //     }
+
+  //     return <ChatInput />
+  // }
+};
