c6f738402e1fd44ceed6cb900021db27be0c7939
priyanshu-09 <priyanshushukla9801@gmail.com>
2024-07-03 20:02:06 +0530

    removing package-dist

--- package-dist/Page.d.ts
+++
@@ -1,3 +0,0 @@
-import { default as React } from 'react';
-
-export declare const Page: React.FC;
--- package-dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Button } from './Button';
-export { ChatInput } from './ChatInput';
-export type { ChatInputProps } from './ChatInput';
