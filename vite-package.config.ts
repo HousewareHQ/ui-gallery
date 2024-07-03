bc116958a1fb00d83091d98fdb7fb541f2420229
priyanshu-09 <priyanshushukla9801@gmail.com>
2024-07-03 18:20:27 +0530

    Adding style to package.json

--- vite-package.config.ts
+++ vite-package.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
 import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   plugins: [
