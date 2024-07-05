c787c2e66db0cfaa11ece64d05c6404f2385a835
priyanshu-09 <priyanshushukla9801@gmail.com>
2024-07-05 11:32:25 +0530

    Package changes for regenerate response

--- package-dist/AIMessageTrendsFunnels.d.ts
+++ package-dist/AIMessageTrendsFunnels.d.ts
@@ -1,6 +1,7 @@
 import { BaseMessage } from './ChatScreenPA';
 
-export declare function AIMessageTrendsFunnels<T extends BaseMessage>({ index, messages, }: {
+export declare function AIMessageTrendsFunnels<T extends BaseMessage>({ index, messages, handleRegenerateResponse, }: {
     index: number;
     messages: T[];
+    handleRegenerateResponse: (userQuery: string, regenerateResponse?: boolean) => void;
 }): import("react/jsx-runtime").JSX.Element;
--- package-dist/ChatScreenPA.d.ts
+++ package-dist/ChatScreenPA.d.ts
@@ -4,7 +4,8 @@ export interface BaseMessage {
 }
 export interface ChatScreenPAProps<T> {
     messages: T[];
-    handleSendFollowupMessage: (userQuery: string) => void;
+    handleSendFollowupMessage: (userQuery: string, regenerateResponse?: boolean) => void;
     isMessageLoading: boolean;
+    setMessages: React.Dispatch<React.SetStateAction<T[]>>;
 }
-export declare function ChatScreenPA<T extends BaseMessage>({ messages, handleSendFollowupMessage, isMessageLoading, }: ChatScreenPAProps<T>): import("react/jsx-runtime").JSX.Element;
+export declare function ChatScreenPA<T extends BaseMessage>({ messages, handleSendFollowupMessage, isMessageLoading, setMessages, }: ChatScreenPAProps<T>): import("react/jsx-runtime").JSX.Element;
