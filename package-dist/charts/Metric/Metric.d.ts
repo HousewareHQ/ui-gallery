ecc1a5a9eae02aee6abf5d8a8608721888023e83
bikram <6bikramsutar@gmail.com>
2024-08-08 14:10:33 +0530

    make change optional on metrics

--- package-dist/charts/Metric/Metric.d.ts
+++ package-dist/charts/Metric/Metric.d.ts
@@ -2,7 +2,7 @@ export type MetricProps = {
     title: string;
     value: number;
     valuePrefix?: string;
-    change: number;
+    change?: number;
     changeType?: "up" | "down";
 };
 export default function Metric({ title, value, change, valuePrefix, changeType }: MetricProps): import("react/jsx-runtime").JSX.Element;
