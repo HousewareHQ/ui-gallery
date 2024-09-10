8e903272014bf79756d8db2db4f84a5cdf608f89
bikram <6bikramsutar@gmail.com>
2024-09-10 16:42:08 +0530

    updated the pie chart theme

--- src/components/charts/Piechart/Piechart.tsx
+++ src/components/charts/Piechart/Piechart.tsx
@@ -15,17 +15,222 @@ export type PiechartProps = {
 export default function Piechart(props: PiechartProps) {
     const chartOptions = useMemo(
         () => ({
-            title: {
+            "color": [
+                "#d87c7c",
+                "#919e8b",
+                "#d7ab82",
+                "#6e7074",
+                "#61a0a8",
+                "#efa18d",
+                "#787464",
+                "#cc7e63",
+                "#724e58",
+                "#4b565b"
+            ],
+            "textStyle": {},
+            "title": {
                 text: props.title,
                 subtext: props.subtext,
-                left: "center"
+                left: "center",
+                "textStyle": {
+                    "color": "#333333"
+                },
+                "subtextStyle": {
+                    "color": "#aaaaaa"
+                }
             },
-            tooltip: {
-                trigger: "item"
+            "line": {
+                "itemStyle": {
+                    "borderWidth": 1
+                },
+                "lineStyle": {
+                    "width": 2
+                },
+                "symbolSize": 4,
+                "symbol": "emptyCircle",
+                "smooth": false
             },
-            legend: {
+
+
+            "categoryAxis": {
+                "axisLine": {
+                    "show": true,
+                    "lineStyle": {
+                        "color": "#333"
+                    }
+                },
+                "axisTick": {
+                    "show": true,
+                    "lineStyle": {
+                        "color": "#333"
+                    }
+                },
+                "axisLabel": {
+                    "show": true,
+                    "color": "#333"
+                },
+                "splitLine": {
+                    "show": false,
+                    "lineStyle": {
+                        "color": [
+                            "#ccc"
+                        ]
+                    }
+                },
+                "splitArea": {
+                    "show": false,
+                    "areaStyle": {
+                        "color": [
+                            "rgba(250,250,250,0.3)",
+                            "rgba(200,200,200,0.3)"
+                        ]
+                    }
+                }
+            },
+            "valueAxis": {
+                "axisLine": {
+                    "show": true,
+                    "lineStyle": {
+                        "color": "#333"
+                    }
+                },
+                "axisTick": {
+                    "show": true,
+                    "lineStyle": {
+                        "color": "#333"
+                    }
+                },
+                "axisLabel": {
+                    "show": true,
+                    "color": "#333"
+                },
+                "splitLine": {
+                    "show": true,
+                    "lineStyle": {
+                        "color": [
+                            "#ccc"
+                        ]
+                    }
+                },
+                "splitArea": {
+                    "show": false,
+                    "areaStyle": {
+                        "color": [
+                            "rgba(250,250,250,0.3)",
+                            "rgba(200,200,200,0.3)"
+                        ]
+                    }
+                }
+            },
+            "logAxis": {
+                "axisLine": {
+                    "show": true,
+                    "lineStyle": {
+                        "color": "#333"
+                    }
+                },
+                "axisTick": {
+                    "show": true,
+                    "lineStyle": {
+                        "color": "#333"
+                    }
+                },
+                "axisLabel": {
+                    "show": true,
+                    "color": "#333"
+                },
+                "splitLine": {
+                    "show": true,
+                    "lineStyle": {
+                        "color": [
+                            "#ccc"
+                        ]
+                    }
+                },
+                "splitArea": {
+                    "show": false,
+                    "areaStyle": {
+                        "color": [
+                            "rgba(250,250,250,0.3)",
+                            "rgba(200,200,200,0.3)"
+                        ]
+                    }
+                }
+            },
+            "timeAxis": {
+                "axisLine": {
+                    "show": true,
+                    "lineStyle": {
+                        "color": "#333"
+                    }
+                },
+                "axisTick": {
+                    "show": true,
+                    "lineStyle": {
+                        "color": "#333"
+                    }
+                },
+                "axisLabel": {
+                    "show": true,
+                    "color": "#333"
+                },
+                "splitLine": {
+                    "show": true,
+                    "lineStyle": {
+                        "color": [
+                            "#ccc"
+                        ]
+                    }
+                },
+                "splitArea": {
+                    "show": false,
+                    "areaStyle": {
+                        "color": [
+                            "rgba(250,250,250,0.3)",
+                            "rgba(200,200,200,0.3)"
+                        ]
+                    }
+                }
+            },
+            "toolbox": {
+                "iconStyle": {
+                    "borderColor": "#999999"
+                },
+                "emphasis": {
+                    "iconStyle": {
+                        "borderColor": "#666666"
+                    }
+                }
+            },
+            "legend": {
                 orient: "vertical",
-                left: "right"
+                left: "right",
+                "textStyle": {
+                    "color": "#333333"
+                }
+            },
+            "tooltip": {
+                trigger: "item",
+                "axisPointer": {
+                    "lineStyle": {
+                        "color": "#cccccc",
+                        "width": 1
+                    },
+                    "crossStyle": {
+                        "color": "#cccccc",
+                        "width": 1
+                    }
+                }
+            },
+            "markPoint": {
+                "label": {
+                    "color": "#eee"
+                },
+                "emphasis": {
+                    "label": {
+                        "color": "#eee"
+                    }
+                }
             },
             series: props.series.map((data, i) => ({
                 name: `Series ${i}`,
